007b2ce12beec61c6c9a7d4c7b0c8ccc4eab7fe7
massprogressive <massprogressive@gmail.com>
2024-08-23 23:12:04 +0300

    Ready to use key generator

--- private-key.py
+++ private-key.py
@@ -6,15 +6,15 @@ from hdwallet.derivations import BIP44Derivation
 from typing import Optional
 
 # Generated previously mnemonic phrase
-MNEMONIC: str = 'here should be your twelve of twenty four length mnemonic seed phrase' # generate_mnemonic(language="english", strength=128)
+MNEMONIC: str = 'here should be your twelve of twenty four length mnemonic seed phrase'
 
 # Secret passphrase/password for mnemonic
 PASSPHRASE: Optional[str] = None  # "strong-passphrase-password" if you have any
 
-# Initialize Cosmos mainnet BIP44HDWallet
+# Initialize Atom mainnet BIP44HDWallet
 bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=AtomMainnet)
 
-# Get Cosmos BIP44HDWallet from mnemonic
+# Get Atom BIP44HDWallet from mnemonic
 bip44_hdwallet.from_mnemonic(
     mnemonic=MNEMONIC, language="english", passphrase=PASSPHRASE
 )
@@ -23,17 +23,16 @@ bip44_hdwallet.clean_derivation()
 
 print("Mnemonic:", bip44_hdwallet.mnemonic())
 
-# Get Cosmos BIP44HDWallet information's from address index
 address_index = 100
 
 # Example of changing account index
 "m/44'/-'/0'/0/100"
 
-# Derivation from Ethereum BIP44 derivation path
+# Derivation from Atom BIP44 derivation path
 bip44_derivation: BIP44Derivation = BIP44Derivation(
     cryptocurrency=AtomMainnet, account=0, change=False, address=address_index
 )
-# Drive Cosmos BIP44HDWallet
+# Drive Atom BIP44HDWallet
 bip44_hdwallet.from_path(path=bip44_derivation)
 
 # Print address_index, path, address and private_key
